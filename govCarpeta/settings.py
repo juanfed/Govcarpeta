7b939f0cec36b6603402d514853d724dc027d6e3
Juan Fernando <juanfed12345@gmail.com>
2023-05-29 18:35:38 -0500

    add more change 3

--- govCarpeta/settings.py
+++ govCarpeta/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-xcz1sa)xs_en)%jv*yjastjthj$*4u-may&+pq+sy%=1t8ampo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -119,11 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static', 'staticfiles_builds')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
